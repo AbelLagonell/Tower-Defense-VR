54b273c64d83639cbfa353e3d6897b8ab232288e
DarkenedRaven <ravencatarina@gmail.com>
2025-04-25 21:33:25 -0400

    SFX

---
+++ Assets/Code/ButtonVR.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonVR : MonoBehaviour
+{
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    private GameObject presser;
+    //private AudioSource sound;
+    bool isPressed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //sound = GetComponent<AudioSource>();
+        isPressed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!isPressed)
+        {
+            button.transform.localPosition = new Vector3(0, -0.05f, 0);
+            presser = other.gameObject;
+            onPress.Invoke();
+            //sound.Play();
+            isPressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == presser)
+        {
+            button.transform.localPosition = new Vector3(0, -0.015f, 0);
+            onRelease.Invoke();
+            isPressed = false;
+        }
+    }
+
+    public void StartWave()
+    {
+        //Start wave
+        //Wave++
+        Debug.Log("Starting Next Wave");
+    }
+}
